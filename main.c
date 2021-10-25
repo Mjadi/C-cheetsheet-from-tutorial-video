5c79ecb73a96ece3ad3f01e87b5a1efd2a2d0de2
Mjadi <85009835+Mjadi@users.noreply.github.com>
2021-10-25 14:50:23 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+// This is a hello world program
+
+/*int main(int argc, char const *argv[])
+{
+	// printf("%s\n", "Hello World!");
+
+	return 0;
+}*/
+
+int remainder(int x, int y) {
+	return x % y;
+}
+
+void main()
+{
+/*
+	int x;
+	int y;
+	x = 89;
+	y = 34;
+	printf("%d\n", x * y);
+	printf("%d\n", x % y);
+	// return 0;
+*/
+
+	/*
+	float a = 45.2;
+	float b = 7.1;
+	char c = 'r';
+
+	printf("%f\n", a * b);
+	printf("%c\n", c);
+
+	*/
+	char i = 'f';
+	/*
+
+	for (int i = 0; i <= 100; ++i)
+	{
+		printf("%d\n", i);
+		
+	}*/
+
+
+	/*
+	while (i < 50) {
+		i++;
+		printf("%d\n", i);
+	}
+	*/
+
+	/*
+	do {
+		printf("%s\n", "C Tutorial");
+	}while (i > 5);
+
+	*/
+
+	/*
+
+	if (i  < 5)
+	{
+		
+		printf("%s\n", "The number is less than 5");
+	}
+
+	else if(i > 5){
+		printf("%s\n", "The number is greater than 5");
+	}
+
+	else {
+		printf("%s\n", "Error");
+	}
+
+	*/
+
+	/*
+	int arr[5] = {5, 4, 3, 2, 1};
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("%d\n", arr[i]);
+	}
+	*/
+
+	/*
+	int x;
+	int y;
+	int *cp;
+	int *xp;
+
+	xp = &y;
+
+	cp = &x;
+
+	
+	printf("%d\n", cp); 
+	printf("%d\n", xp);*/
+	int y = 34;
+
+	scanf("%d", &y);
+	printf("%d\n", y);
+
+
+
+
+
+
+}
